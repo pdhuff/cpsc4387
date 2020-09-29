e5351fd2f393cabdd6779ceac177113a54a49f04
Philip Huff <pdhuff@ualr.edu>
2020-09-28 20:25:52 -0500

    Adding sample cloud run app

--- project1/cloud-functions/main.py
+++ project1/cloud-functions/main.py
@@ -21,3 +21,29 @@ def cloud_fn_stop_all_servers(event, context):
     if 'items' in result:
         for vm_instance in result['items']:
             compute.instances().stop(project=project, zone=zone, instance=vm_instance["name"]).execute()
+
+
+def cloud_fn_your_cloud_function(event, context):
+    """
+    This is your function
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    Returns:
+        A success status
+    """
+
+    action = event['attributes']['action'] if 'action' in event['attributes'] else None
+
+    if not action:
+        print(f'No action provided in cloud_fn_manage_server for published message.')
+        return
+
+    if action == "build":
+        print("Replace this with a function to build the cloud server")
+    elif action == "bucket":
+        print("Replace this with a function to create the cloud bucket.")
---
+++ project1/cloud-run-template/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from google.cloud import pubsub_v1
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def test_page():
+    project = "name_of_your_gcp_project"
+    if request.method == 'POST':
+        pubsub_topic = "build_server_or_whatever_you_want"
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(project, pubsub_topic)
+        future = publisher.publish(topic_path, data=b'Some text noone will read', action="build")
+        print(future.result())
+    page_template = 'base_template.html'
+    return render_template(page_template)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
